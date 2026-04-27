87d54789717b44aa63d1b55b3c7c920f609c2778
Mert ALTINBAS <lordonmert@gmail.com>
2026-04-28 01:07:52 +0200

    Added polymorphic method tests in Main class

--- src/MP3/Main.java
+++ src/MP3/Main.java
@@ -2,6 +2,10 @@ package MP3;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Testing polymorphic method
+        Person doctor1 = new Doctor(0.85f);
+        Person patient1 = new Patient(0.80f);
+        System.out.println(doctor1.calculateFinalBill(1200f));
+        System.out.println(patient1.calculateFinalBill(1200f));
     }
 }
